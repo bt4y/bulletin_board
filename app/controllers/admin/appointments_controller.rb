a090e91afc508a99a8238abf919f97c7e1827f36
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-23 12:29:02 +0200

    Add some actions to the Admin::AppointmentsController.

--- app/controllers/admin/appointments_controller.rb
+++ app/controllers/admin/appointments_controller.rb
@@ -1,2 +1,36 @@
 class Admin::AppointmentsController < ApplicationController
+  def index
+    @appointments = Appointment.all
+  end
+  
+  def new
+    @appointment = Appointment.new
+  end
+  
+  def create
+    @appointment = Appointment.new(params[:appointment])
+    if @appointment.save
+      redirect_to :action => :index
+    else
+      render :action => :new
+    end
+  end
+  
+  def edit
+    @appointment = Appointment.find(params[:id])
+  end
+  
+  def update
+    @appointment = Appointment.find(params[:id])
+    if @appointment.update_attributes(params[:appointment])
+      redirect_to :action => :index
+    else
+      render :action => :edit
+    end
+  end
+  
+  def destroy
+    Appointment.destroy(params[:id])
+    redirect_to :action => :index
+  end
 end
