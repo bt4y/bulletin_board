13b29666151fb09cab17533d2607bd33720d1a8d
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-27 23:57:52 +0200

    Only show recent appointments on the frontpage.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,5 @@
 class MainController < ApplicationController
   def index
-    @appointments = Appointment.all
+    @appointments = Appointment.recent
   end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,8 @@
 class Appointment < ActiveRecord::Base
+  named_scope :recent, lambda {
+    { :conditions => [ "show_at < :now AND hide_at > :now", { :now => Time.now } ],
+      :order      => "hide_at DESC" }
+  }
+  
+  validates_presence_of :title, :body, :hide_at, :show_at
 end
