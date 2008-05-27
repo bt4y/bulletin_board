9e01d0685c4299920360389c255c14c3a59477ce
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-27 23:58:26 +0200

    Make the clock on the frontpage work.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,34 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+if (!Date.now) {
+    Date.now = function() {
+        return (new Date()).getTime();
+    };
+}
+
+function updateClock() {
+  var el = $("uhr");
+  var time = new Date();
+  
+  var hours = time.getHours(),
+      minutes = time.getMinutes(),
+      seconds = time.getSeconds();
+  
+  if (hours < 10)
+    hours = "0" + hours;
+
+  if (minutes < 10)
+    minutes = "0" + minutes;
+
+  if (seconds < 10)
+    seconds = "0" + seconds;
+
+  
+  el.update("<p>" + hours + ":" + minutes + ":" + seconds + "</p>");
+}
+
+document.observe("dom:loaded", function(ev) {
+  if ($("uhr")) {
+    window.setInterval(updateClock, 1000);
+  }
+})
