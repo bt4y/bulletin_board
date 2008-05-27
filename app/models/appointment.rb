14a090c3a28abf70e7e5a665a5c1d95f360cfd2f
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-05-28 00:03:51 +0200

    Update the model to reflect database changes.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,8 +1,8 @@
 class Appointment < ActiveRecord::Base
   named_scope :recent, lambda {
-    { :conditions => [ "show_at < :now AND hide_at > :now", { :now => Time.now } ],
-      :order      => "hide_at DESC" }
+    { :conditions => [ "show_on < :now AND hide_on > :now", { :now => Time.now } ],
+      :order      => "hide_on DESC" }
   }
   
-  validates_presence_of :title, :body, :hide_at, :show_at
+  validates_presence_of :title, :body, :hide_on, :show_on
 end
