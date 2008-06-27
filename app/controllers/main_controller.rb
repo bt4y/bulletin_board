cb373a45d32fddf0b6a62b25867ae08fd02c08e5
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-06-27 17:51:02 +0200

    StandInProcessor für den Lehrer-Bereich aktualisiert.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,8 @@
 class MainController < ApplicationController
   def index
     @appointments = Appointment.recent
-    @stand_ins = StandInProcessor.new(File.read(RAILS_ROOT + "/public/tag_1.txt"))
+    @vertretungen_heute = StandInProcessor.new(File.read(RAILS_ROOT + "/public/tag_1.txt"))
+    @vertretungen_morgen = StandInProcessor.new(File.read(RAILS_ROOT + "/public/tag_2.txt"))
   end
   
   def lehrer
--- app/models/stand_in_processor.rb
+++ app/models/stand_in_processor.rb
@@ -1,29 +1,45 @@
 class StandInProcessor
-	attr_reader :text
-	
-	def initialize(text)
-	  @text = {}
-	  
-	  vertretungen = false
-	  
-		text.each_line do |line|
-		  if line =~ /^((\d{1,2}\w)|(\d{2}))/
-		    vertretungen = $1
-	    elsif vertretungen
-		    if line =~ /Ersatzraumplan/
-		      return
-		    else
-		      (@text[vertretungen] ||= []) << line.split(/\t/)
-	      end
-	    end
-	  end
-	end
-	
-	def sort_by(&block)
-	  @text.sort_by(&block)
+  attr_reader :vertretungen
+  
+  def initialize(text)
+    @vertretungen = []
+    
+    klasse = nil
+    
+    text.each_line do |line|
+      if line =~ /^((\d{1,2}\w)|(\d{2}))/
+        klasse = $1
+      elsif klasse
+        if line =~ /Ersatzraumplan/
+          # Wir sind am Ende der Liste angekommen
+          return
+        else
+          line_ary = line.split(/\t/)
+          
+          @vertretungen << {
+            :klasse     => klasse,
+            :stunde     => line_ary[0],
+            :lehrer     => line_ary[1],
+            :fach       => line_ary[2],
+            :raum       => line_ary[3],
+            :vertretung => line_ary[6],
+            :info       => line_ary[7]
+          }
+        end
+      end
+    end
   end
-	
-	def each(&block)
-	  @text.each(&block)
+  
+  def each_sorted_by_class(&block)
+    @vertretungen.sort do |a, b|
+      a, b = a[:klasse], b[:klasse]
+      
+      if a != b && a.to_i == b.to_i
+        p a[-1] <=> b[-1]
+        a[-1] <=> b[-1]
+      else
+        a.to_i <=> b.to_i
+      end
+    end.each(&block)
   end
 end
