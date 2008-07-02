279e367ae5217deb5497347a48cd7e8b9b9166d2
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-07-02 09:42:02 +0200

    Die Informationen werden jetzt so angezeigt, dass die als nächstes endende Information an oberster Stelle angezeigt wird.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,7 @@
 class Appointment < ActiveRecord::Base
   named_scope :recent, lambda {
     { :conditions => [ "show_on < :now AND hide_on > :now", { :now => Time.now } ],
-      :order      => "hide_on DESC" }
+      :order      => "hide_on ASC" }
   }
   
   validates_presence_of :title, :body, :hide_on, :show_on
