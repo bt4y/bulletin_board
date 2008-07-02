89d1e6de8c43aef4cfa9434fc548cf81822f33ac
Arthur Schreiber <schreiber.arthur@gmail.com>
2008-07-02 10:02:44 +0200

    Ein paar Named-Scopes zum Information-Model hinzugefügt.

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,2 +1,7 @@
 class Information < ActiveRecord::Base
+  named_scope :teacher, :conditions => { :teachers_only => true }
+  named_scope :general, :conditions => { :teachers_only => false }
+  named_scope :recent,  lambda { {
+      :conditions => [ "show_on < :now AND hide_on > :now", :now => Time.now],
+      :order      => "hide_on ASC" } }
 end
